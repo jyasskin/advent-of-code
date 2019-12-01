255be0eee3d2a9b84c53de056d2674ea12cc2d58
Jeffrey Yasskin <jyasskin@gmail.com>
2019-12-01 14:16:19 -0800

    rustfmt is a thing.

--- 2019/1/part1.rs
+++ 2019/1/part1.rs
@@ -2,11 +2,14 @@ use std::io;
 use std::io::BufRead;
 
 fn main() {
-  let mut total_fuel = 0;
-  for line in io::stdin().lock().lines() {
-    let mass : u32 = line.expect("Can't get lines from stdin").parse().expect("Line wasn't an integer");
-    let fuel = mass / 3 - 2;
-    total_fuel += fuel;
-  }
-  println!("{}", total_fuel);
+    let mut total_fuel = 0;
+    for line in io::stdin().lock().lines() {
+        let mass: u32 = line
+            .expect("Can't get lines from stdin")
+            .parse()
+            .expect("Line wasn't an integer");
+        let fuel = mass / 3 - 2;
+        total_fuel += fuel;
+    }
+    println!("{}", total_fuel);
 }
