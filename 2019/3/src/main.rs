11b29b55a45b24570ea12e82c7d5aaa9935805bb
Jeffrey Yasskin <jyasskin@gmail.com>
2019-12-02 22:54:30 -0800

    Day 3 part 2

--- 2019/3/src/main.rs
+++ 2019/3/src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ord;
 use std::collections::HashMap;
 use std::io;
 use std::io::BufRead;
@@ -9,17 +10,17 @@ fn main() {
         panic!("Invalid input: {:?}", path_texts);
     }
     println!(
-        "Part 1: {}",
+        "Part 1: Min distance: {}",
         min_distance(path_texts[0].as_str(), path_texts[1].as_str())
+    );
+    println!(
+        "Part 2: Min steps: {}",
+        min_steps(path_texts[0].as_str(), path_texts[1].as_str())
     );
 }
 
 fn min_distance(path0: &str, path1: &str) -> usize {
-    let path0 = Path::parse(path0);
-    let path1 = Path::parse(path1);
-    let mut grid = Grid::new();
-    path0.follow(&mut grid, 0);
-    path1.follow(&mut grid, 1);
+    let grid = initialize_grid(path0, path1);
     let mut min = usize::max_value();
     for (position, wires) in &grid.points {
         let distance = position.manhattan_distance();
@@ -28,6 +29,28 @@ fn min_distance(path0: &str, path1: &str) -> usize {
         }
     }
     return min;
+}
+
+fn min_steps(path0: &str, path1: &str) -> usize {
+    let grid = initialize_grid(path0, path1);
+    let mut min = usize::max_value();
+    for (_, wires) in &grid.points {
+        if let Some(steps) = wires.total_steps() {
+            if steps > 0 && steps < min {
+                min = steps;
+            }
+        }
+    }
+    return min;
+}
+
+fn initialize_grid(path0: &str, path1: &str) -> Grid {
+    let path0 = Path::parse(path0);
+    let path1 = Path::parse(path1);
+    let mut grid = Grid::new();
+    path0.follow(&mut grid, 0);
+    path1.follow(&mut grid, 1);
+    return grid;
 }
 
 enum Direction {
@@ -47,7 +70,8 @@ impl Direction {
         }
     }
 
-    fn move1(&self, position: &mut Point) {
+    fn move1(&self, position: &mut Point, step_count: &mut usize) {
+        *step_count += 1;
         match &self {
             Direction::Right => position.x += 1,
             Direction::Down => position.y -= 1,
@@ -71,11 +95,11 @@ impl Step {
         };
     }
 
-    fn follow(&self, position: &mut Point, grid: &mut Grid, wirenum: u8) {
-        grid.set(*position, wirenum);
+    fn follow(&self, position: &mut Point, grid: &mut Grid, wirenum: u8, step_count: &mut usize) {
+        grid.set(*position, wirenum, *step_count);
         for _ in 0..self.distance {
-            self.direction.move1(position);
-            grid.set(*position, wirenum);
+            self.direction.move1(position, step_count);
+            grid.set(*position, wirenum, *step_count);
         }
     }
 }
@@ -93,8 +117,9 @@ impl Path {
 
     fn follow(&self, grid: &mut Grid, wirenum: u8) {
         let mut position = Point { x: 0, y: 0 };
+        let mut step_count: usize = 0;
         for step in &self.steps {
-            step.follow(&mut position, grid, wirenum);
+            step.follow(&mut position, grid, wirenum, &mut step_count);
         }
     }
 }
@@ -110,18 +135,34 @@ impl Point {
     }
 }
 
+fn set_min(steps: &mut Option<usize>, new_steps: usize) {
+    *steps = steps.map_or(Some(new_steps), |old| Some(old.min(new_steps)));
+}
+
 struct WiresPresent {
-    bits: u8,
+    steps0: Option<usize>,
+    steps1: Option<usize>,
 }
 impl WiresPresent {
     fn new() -> WiresPresent {
-        WiresPresent { bits: 0 }
+        WiresPresent {
+            steps0: None,
+            steps1: None,
+        }
     }
-    fn set(&mut self, wirenum: u8) {
-        self.bits |= 1 << wirenum;
+    fn set(&mut self, wirenum: u8, steps: usize) {
+        match wirenum {
+            0 => set_min(&mut self.steps0, steps),
+            1 => set_min(&mut self.steps1, steps),
+            _ => panic!("Unexpected wire number: {}", wirenum),
+        }
     }
     fn both_present(&self) -> bool {
-        return self.bits == 3;
+        return self.steps0.is_some() && self.steps1.is_some();
+    }
+    fn total_steps(&self) -> Option<usize> {
+        self.steps0
+            .and_then(|steps0| self.steps1.map(|steps1| steps0 + steps1))
     }
 }
 
@@ -135,11 +176,11 @@ impl Grid {
         }
     }
 
-    fn set(&mut self, position: Point, wirenum: u8) {
+    fn set(&mut self, position: Point, wirenum: u8, step_count: usize) {
         self.points
             .entry(position)
             .or_insert(WiresPresent::new())
-            .set(wirenum);
+            .set(wirenum, step_count);
     }
 }
 
@@ -156,11 +197,25 @@ mod tests {
             159
         );
         assert_eq!(
+            min_steps(
+                "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+                "U62,R66,U55,R34,D71,R55,D58,R83"
+            ),
+            610
+        );
+        assert_eq!(
             min_distance(
                 "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
                 "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
             ),
             135
+        );
+        assert_eq!(
+            min_steps(
+                "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+                "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+            ),
+            410
         );
     }
 }
