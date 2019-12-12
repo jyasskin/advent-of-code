7628133ac140ac045b383b63783e3d8168f1008d
Jeffrey Yasskin <jyasskin@gmail.com>
2019-12-11 20:55:40 -0800

    Day 10 part 2

--- 2019/10/src/main.rs
+++ 2019/10/src/main.rs
@@ -1,12 +1,18 @@
+use advent_util::*;
 use ndarray;
-use num_integer::Integer;
+use num::Integer;
+use std::convert::TryFrom;
 use std::io;
 use std::io::Read;
 
 fn main() {
     let input = input();
 
-    println!("Part 1: {:?}", locate_asteroid_detector(&input));
+    let map = parse_map(&input);
+
+    let (station, seen) = locate_asteroid_detector(&map);
+    println!("Part 1: {:?}", (station, seen));
+    println!("Part 2: {:?}", vaporization_order(&station, map)[199]);
 }
 
 fn input() -> String {
@@ -17,17 +23,32 @@ fn input() -> String {
     input
 }
 
-#[derive(PartialEq, Eq, Hash, Debug)]
-struct Point {
-    x: isize,
-    y: isize,
-}
-fn point(x: isize, y: isize) -> Point {
-    Point { x, y }
+type Point2 = euclid::default::Point2D<i32>;
+type Vector2 = euclid::default::Vector2D<i32>;
+fn point(x: i32, y: i32) -> Point2 {
+    Point2::new(x, y)
 }
 
-fn locate_asteroid_detector(map: &str) -> (Point, i32) {
-    let map = parse_map(map);
+fn print_map(map: &ndarray::Array2<i32>, laser: Option<&Point2>) {
+    for row in 0..map.nrows() {
+        for col in 0..map.ncols() {
+            print!(
+                "{}",
+                if Some(&point(col as i32, row as i32)) == laser {
+                    "*"
+                } else if map[(row, col)] == 1 {
+                    "#"
+                } else {
+                    " "
+                }
+            );
+        }
+        println!();
+    }
+    println!();
+}
+
+fn locate_asteroid_detector(map: &ndarray::Array2<i32>) -> (Point2, i32) {
     let mut max_seen = 0;
     let mut max_pos = point(0, 0);
     for row in 0..map.nrows() {
@@ -37,7 +58,7 @@ fn locate_asteroid_detector(map: &str) -> (Point, i32) {
                 let seen = compute_seen(row, col, &map);
                 if seen > max_seen {
                     max_seen = seen;
-                    max_pos = point(col as isize, row as isize);
+                    max_pos = point(col as i32, row as i32);
                 }
             }
         }
@@ -61,7 +82,7 @@ fn parse_map(s: &str) -> ndarray::Array2<i32> {
     ndarray::Array2::from_shape_fn((rows.len(), rows[0].len()), |(row, col)| rows[row][col])
 }
 
-fn ints_between(mut from: isize, to: isize, step: Option<isize>) -> Vec<isize> {
+fn ints_between(mut from: i32, to: i32, step: Option<i32>) -> Vec<i32> {
     let step = match step {
         Some(step) => step,
         None => {
@@ -81,7 +102,7 @@ fn ints_between(mut from: isize, to: isize, step: Option<isize>) -> Vec<isize> {
     result
 }
 
-fn points_between(from: Point, to: Point) -> Vec<Point> {
+fn points_between(from: Point2, to: Point2) -> Vec<Point2> {
     if from.x == to.x {
         return ints_between(from.y, to.y, None)
             .iter()
@@ -109,10 +130,13 @@ fn compute_seen(row: usize, col: usize, map: &ndarray::Array2<i32>) -> i32 {
     for test_row in 0..map.nrows() {
         'asteroids: for test_col in 0..map.ncols() {
             if map[(test_row, test_col)] == 1 && (test_row != row || test_col != col) {
-                for Point { x, y } in points_between(
-                    point(col as isize, row as isize),
-                    point(test_col as isize, test_row as isize),
-                ) {
+                for (x, y) in points_between(
+                    point(col as i32, row as i32),
+                    point(test_col as i32, test_row as i32),
+                )
+                .into_iter()
+                .map(|p| p.to_tuple())
+                {
                     if map[(y as usize, x as usize)] == 1 {
                         continue 'asteroids;
                     }
@@ -122,6 +146,97 @@ fn compute_seen(row: usize, col: usize, map: &ndarray::Array2<i32>) -> i32 {
         }
     }
     num_seen
+}
+
+fn vaporization_order(laser: &Point2, mut map: ndarray::Array2<i32>) -> Vec<Point2> {
+    let max_distance: i32 = *[
+        laser.x,
+        laser.y,
+        map.nrows() as i32 - laser.y,
+        map.ncols() as i32 - laser.x,
+    ]
+    .iter()
+    .max()
+    .unwrap();
+    let angles: Vec<Vector2> = stern_brocot_sequence(max_distance)
+        .iter()
+        .map(|angle| Vector2::new(*angle.numer(), *angle.denom()))
+        .filter(|angle| angle.x <= max_distance && angle.y <= max_distance)
+        .collect();
+    let mut result = vec![];
+    while map.sum() > 1 {
+        if let Some(asteroid) = pew(&Vector2::new(0, -1), laser, &mut map) {
+            result.push(asteroid);
+        }
+        // Top-right quadrant.
+        for angle in angles.iter() {
+            if let Some(asteroid) = pew(&Vector2::new(angle.x, -angle.y), laser, &mut map) {
+                result.push(asteroid);
+            }
+        }
+        if let Some(asteroid) = pew(&Vector2::new(1, 0), laser, &mut map) {
+            result.push(asteroid);
+        }
+        // Bottom-right quadrant.
+        for angle in angles.iter().rev() {
+            if let Some(asteroid) = pew(&Vector2::new(angle.x, angle.y), laser, &mut map) {
+                result.push(asteroid);
+            }
+        }
+        if let Some(asteroid) = pew(&Vector2::new(0, 1), laser, &mut map) {
+            result.push(asteroid);
+        }
+        // Bottom-left quadrant.
+        for angle in angles.iter() {
+            if let Some(asteroid) = pew(&Vector2::new(-angle.x, angle.y), laser, &mut map) {
+                result.push(asteroid);
+            }
+        }
+        if let Some(asteroid) = pew(&Vector2::new(-1, 0), laser, &mut map) {
+            result.push(asteroid);
+        }
+        // Top-left quadrant.
+        for angle in angles.iter().rev() {
+            if let Some(asteroid) = pew(&Vector2::new(-angle.x, -angle.y), laser, &mut map) {
+                result.push(asteroid);
+            }
+        }
+    }
+    result
+}
+
+fn pew(angle: &Vector2, laser: &Point2, map: &mut ndarray::Array2<i32>) -> Option<Point2> {
+    for multiple in multiples_to(angle, map.nrows().max(map.ncols()) as i32 + 1) {
+        let try_asteroid = *laser + multiple;
+        if try_asteroid.x < 0 || try_asteroid.y < 0 {
+            break;
+        }
+        match map.get_mut((
+            usize::try_from(try_asteroid.y).unwrap(),
+            usize::try_from(try_asteroid.x).unwrap(),
+        )) {
+            None => break,
+            Some(point) => {
+                if *point > 0 {
+                    *point = 0;
+                    return Some(try_asteroid);
+                }
+            }
+        }
+    }
+    None
+}
+
+fn multiples_to(v: &Vector2, max: i32) -> Vec<Vector2> {
+    let mut result = vec![];
+    for mul in 1.. {
+        let next = Vector2::new(v.x * mul, v.y * mul);
+        if next.x.abs() > max || next.y.abs() > max {
+            break;
+        }
+        result.push(next);
+    }
+    result
 }
 
 #[cfg(test)]
@@ -172,10 +287,14 @@ mod tests {
         );
     }
 
+    fn test_locate(s: &str) -> (Point2, i32) {
+        locate_asteroid_detector(&parse_map(s))
+    }
+
     #[test]
     fn examples() {
         assert_eq!(
-            locate_asteroid_detector(
+            test_locate(
                 ".#..#
 .....
 #####
@@ -185,7 +304,7 @@ mod tests {
             (point(3, 4), 8)
         );
         assert_eq!(
-            locate_asteroid_detector(
+            test_locate(
                 "......#.#.
 #..#.#....
 ..#######.
@@ -200,7 +319,7 @@ mod tests {
             (point(5, 8), 33)
         );
         assert_eq!(
-            locate_asteroid_detector(
+            test_locate(
                 "#.#...#.#.
 .###....#.
 .#....#...
@@ -215,7 +334,7 @@ mod tests {
             (point(1, 2), 35)
         );
         assert_eq!(
-            locate_asteroid_detector(
+            test_locate(
                 ".#..#..###
 ####.###.#
 ....###.#.
@@ -230,7 +349,7 @@ mod tests {
             (point(6, 3), 41)
         );
         assert_eq!(
-            locate_asteroid_detector(
+            test_locate(
                 ".#..##.###...#######
 ##.############..##.
 .#.######.########.#
@@ -254,5 +373,44 @@ mod tests {
             ),
             (point(11, 13), 210)
         );
+    }
+
+    #[test]
+    fn test_vaporization() {
+        let map = parse_map(
+            ".#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##",
+        );
+        let order = vaporization_order(&point(11, 13), map);
+        assert_eq!(order[0], point(11, 12));
+        assert_eq!(order[1], point(12, 1));
+        assert_eq!(order[2], point(12, 2));
+        assert_eq!(order[9], point(12, 8));
+        assert_eq!(order[19], point(16, 0));
+        assert_eq!(order[49], point(16, 9));
+        assert_eq!(order[99], point(10, 16));
+        assert_eq!(order[198], point(9, 6));
+        assert_eq!(order[199], point(8, 2));
+        assert_eq!(order[200], point(10, 9));
+        assert_eq!(order[298], point(11, 1));
+        assert_eq!(order.len(), 299);
     }
 }
---
+++ 2019/advent_util/src/lib.rs
@@ -0,0 +1,62 @@
+use itertools::Itertools;
+pub use num::rational::Rational32;
+
+// Returns the in-order traversal of the Stern-Brocot tree down to the layer'th
+// layer, which does not include the 0/1 and 1/0 elements at the edges of the
+// tree.
+//
+// This is a sorted list of rational numbers that is a superset of all rationals
+// with a non-negative numerator and denominator <= layer.
+pub fn stern_brocot_sequence(layer: i32) -> Vec<Rational32> {
+    let mut result = vec![Rational32::new_raw(0, 1), Rational32::new_raw(1, 0)];
+    for _ in 1..=layer {
+        let first = result.iter();
+        let mut second = result.iter();
+        second.next();
+        result =
+            result
+                .iter()
+                .copied()
+                .interleave(first.zip(second).map(|(f, s)| {
+                    Rational32::new_raw(f.numer() + s.numer(), f.denom() + s.denom())
+                }))
+                .collect();
+    }
+    result[1..result.len() - 1].into()
+}
+
+pub fn frac(n: i32, d: i32) -> Rational32 {
+    Rational32::new(n, d)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn stern_brocot() {
+        assert_eq!(stern_brocot_sequence(0), vec![]);
+        assert_eq!(stern_brocot_sequence(1), vec![frac(1, 1)]);
+        assert_eq!(
+            stern_brocot_sequence(2),
+            vec![frac(1, 2), frac(1, 1), frac(2, 1)]
+        );
+        assert_eq!(
+            stern_brocot_sequence(3),
+            vec![
+                frac(1, 3),
+                frac(1, 2),
+                frac(2, 3),
+                frac(1, 1),
+                frac(3, 2),
+                frac(2, 1),
+                frac(3, 1)
+            ]
+        );
+
+        let sb30 = stern_brocot_sequence(20);
+        for i in 1..sb30.len() {
+            assert!(sb30[i - 1] < sb30[i]);
+        }
+    }
+}
